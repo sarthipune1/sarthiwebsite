99b3d344707594c8ad5bbadbcf024a6f1b75c9f5
Akshay Kumar <akshay2796@gmail.com>
2021-06-15 02:33:20 +0530

    Fixed notice and strength area

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -38,4 +38,39 @@ export class HomeComponent implements OnInit {
     },
     nav: true,
   };
+
+  noticeData = [
+    {
+      date: {
+        month: 'May',
+        date: 31,
+      },
+      title: 'Memorial day holiday 2021',
+      time: '12:00 AM',
+    },
+    {
+      date: {
+        month: 'June',
+        date: 11,
+      },
+      title: 'Summer A 2021 classes (4 & 8 week) begins',
+      time: '12:00 AM',
+    },
+    {
+      date: {
+        month: 'Sep',
+        date: 25,
+      },
+      title: 'Summer A 2022',
+      time: '12:00 AM',
+    },
+    {
+      date: {
+        month: 'May',
+        date: 31,
+      },
+      title: 'Memorial day holiday 2021',
+      time: '12:00 AM',
+    },
+  ];
 }
