a81146407284434d6f1e0f29ef1757efd3c9260d
Ryan Singh <ryan97singh@gmail.com>
2021-06-17 03:14:00 +0530

    Completed accounts page

--- src/app/notice/notice.module.ts
+++ src/app/notice/notice.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexModule, FlexLayoutModule } from '@angular/flex-layout';
+import {NoticeComponent} from "./notice.component";
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [NoticeComponent],
+  exports:[NoticeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FlexModule,
+    FlexLayoutModule
   ]
 })
 export class NoticeModule { }
