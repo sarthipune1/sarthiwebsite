dbf7edf4c51f90cf6b1343569ac6df8280909d60
Ryan Singh <ryan97singh@gmail.com>
2021-06-19 02:35:11 +0530

    added announcemnets

--- src/app/notice/notice.component.ts
+++ src/app/notice/notice.component.ts
@@ -11,5 +11,115 @@ export class NoticeComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  noticeData = [
+    {
+      date: {
+        month: 'May',
+        date: 31,
+      },
+      title: 'Memorial day holiday 2021',
+      time: '12:00 AM',
+    },
+    {
+      date: {
+        month: 'June',
+        date: 11,
+      },
+      title: 'Summer A 2021 classes (4 & 8 week) begins',
+      time: '12:00 AM',
+    },
+    {
+      date: {
+        month: 'Sep',
+        date: 25,
+      },
+      title: 'Summer A 2022',
+      time: '12:00 AM',
+    },
+    {
+      date: {
+        month: 'May',
+        date: 31,
+      },
+      title: 'Memorial day holiday 2021',
+      time: '12:00 AM',
+    },
+  ];
+  strengthData = [
+    {
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },{
+      icon: "announcements.png",
+      title: '6th semester result out!',
+      description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry',
+    },
+
+  ];
 
 }
