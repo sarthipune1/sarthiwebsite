de83fafe86a120a33b7996afb7e86ff1ca53137a
Akshay Kumar <akshay2796@gmail.com>
2021-08-16 01:48:22 +0530

    Added FormBuilder

--- src/app/events/events.component.ts
+++ src/app/events/events.component.ts
@@ -3,13 +3,48 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-events',
   templateUrl: './events.component.html',
-  styleUrls: ['./events.component.scss']
+  styleUrls: ['./events.component.scss'],
 })
 export class EventsComponent implements OnInit {
+  public form: Object = {
+    components: [
+      {
+        label: 'First Name',
+        placeholder: 'Enter your first name',
+        tableView: true,
+        key: 'firstName',
+        type: 'textfield',
+        input: true,
+      },
+      {
+        type: 'button',
+        label: 'Submit',
+        key: 'submit',
+        disableOnInvalid: true,
+        input: true,
+        tableView: false,
+      },
+    ],
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onChange(event) {
+    // this.jsonElement.nativeElement.innerHTML = '';
+    // this.jsonElement.nativeElement.appendChild(
+    //   document.createTextNode()
+    // );
+    // this.refreshForm.emit({
+    //   property: 'form',
+    //   value: event.form,
+    // });
+
+    console.log(JSON.stringify(event.form, null, 4));
   }
 
+  onSubmit(event) {
+    console.log('Submitting', event);
+  }
 }
