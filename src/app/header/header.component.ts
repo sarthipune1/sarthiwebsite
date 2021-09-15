e6d8b3634acebe33e1142762008661d2ef34f452
Akshay Kumar <akshay2796@gmail.com>
2021-09-15 05:51:07 +0530

    Prod build for demo

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,103 +1,103 @@
 import {
-  Component,
-  HostListener,
-  Input,
-  OnChanges,
-  OnInit,
+	Component,
+	HostListener,
+	Input,
+	OnChanges,
+	OnInit,
 } from '@angular/core';
 import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss'],
+	selector: 'app-header',
+	templateUrl: './header.component.html',
+	styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit, OnChanges {
-  @Input() route: string;
+	@Input() route: string;
 
-  constructor() {}
+	constructor() {}
 
-  ngOnChanges(): void {
-    console.log(this.route);
-  }
+	ngOnChanges(): void {
+		console.log(this.route);
+	}
 
-  ngOnInit(): void {}
-  myFunction() {
-    document.getElementById('myDropdown')?.classList.toggle('show');
-  }
+	ngOnInit(): void {}
+	myFunction() {
+		document.getElementById('myDropdown')?.classList.toggle('show');
+	}
 
-  isToggled = true;
+	isToggled = false;
 
-  menu = [
-    {
-      name: 'Home',
-      path: '/home',
-    },
-    {
-      name: 'About Us',
-      path: '/about-us',
-    },
-    {
-      name: 'Department',
-      path: '/department',
-      sublist: [
-        {
-          name: 'IT Department',
-          path: '/department/it',
-        },
-        {
-          name: 'Women Enpowerment',
-          path: '/department/women-empowerment',
-        },
-      ],
-    },
-    {
-      name: 'Accounts',
-      path: '/accounts',
-    },
-    {
-      name: 'Notice Board',
-      path: '/notices',
-    },
-    {
-      name: 'Reports',
-      path: '/reports',
-    },
-    {
-      name: 'Caste Certificate',
-      path: '/caste-certificate',
-    },
-    {
-      name: 'Awards',
-      path: '/awards',
-    },
-    // {
-    //   name: 'Events',
-    //   path: '/events',
-    // },
-    {
-      name: 'Photo Gallery',
-      path: '/photo-gallery',
-    },
-    {
-      name: 'RTI',
-      path: '/rti',
-    },
-    {
-      name: 'Board of Directors',
-      path: '/board-of-directors',
-    },
-    {
-      name: 'Contact Us',
-      path: '/contact-us',
-    },
-  ];
+	menu = [
+		{
+			name: 'Home',
+			path: '/home',
+		},
+		{
+			name: 'About Us',
+			path: '/about-us',
+		},
+		{
+			name: 'Department',
+			path: '/department',
+			sublist: [
+				{
+					name: 'IT Department',
+					path: '/department/it',
+				},
+				{
+					name: 'Women Enpowerment',
+					path: '/department/women-empowerment',
+				},
+			],
+		},
+		{
+			name: 'Accounts',
+			path: '/accounts',
+		},
+		{
+			name: 'Notice Board',
+			path: '/notices',
+		},
+		{
+			name: 'Reports',
+			path: '/reports',
+		},
+		{
+			name: 'Caste Certificate',
+			path: '/caste-certificate',
+		},
+		{
+			name: 'Awards',
+			path: '/awards',
+		},
+		// {
+		//   name: 'Events',
+		//   path: '/events',
+		// },
+		{
+			name: 'Photo Gallery',
+			path: '/photo-gallery',
+		},
+		{
+			name: 'RTI',
+			path: '/rti',
+		},
+		{
+			name: 'Board of Directors',
+			path: '/board-of-directors',
+		},
+		{
+			name: 'Contact Us',
+			path: '/contact-us',
+		},
+	];
 
-  faIcons = {
-    faBars,
-    faTimes,
-  };
-  setToggled() {
-    this.isToggled = !this.isToggled;
-  }
+	faIcons = {
+		faBars,
+		faTimes,
+	};
+	setToggled() {
+		this.isToggled = !this.isToggled;
+	}
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,95 +1,119 @@
-import {Component, OnInit} from '@angular/core';
-import {OwlOptions} from 'ngx-owl-carousel-o';
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  customOptions: OwlOptions = {
-    loop: true,
-    mouseDrag: true,
-    touchDrag: true,
-    pullDrag: true,
-    dots: false,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    navSpeed: 100,
-    fluidSpeed: true,
-    lazyLoad: true,
-    navText: ['', ''],
-    responsive: {
-      0: {
-        items: 1,
-      },
-      400: {
-        items: 1,
-      },
-      740: {
-        items: 1,
-      },
-      940: {
-        items: 1,
-      },
-    },
-    nav: true,
-  };
-  noticeData = [
-    {
-      date: {
-        month: 'May',
-        date: 31,
-      },
-      title: 'Memorial day holiday 2021',
-      time: '12:00 AM',
-    },
-    {
-      date: {
-        month: 'June',
-        date: 11,
-      },
-      title: 'Summer A 2021 classes (4 & 8 week) begins',
-      time: '12:00 AM',
-    },
-    {
-      date: {
-        month: 'Sep',
-        date: 25,
-      },
-      title: 'Summer A 2022',
-      time: '12:00 AM',
-    },
-    {
-      date: {
-        month: 'May',
-        date: 31,
-      },
-      title: 'Memorial day holiday 2021',
-      time: '12:00 AM',
-    },
-  ];
-  strengthData = [
-    {
-      icon: "strength-1.png",
-      title: 'EXCELLENT FALCUTY',
-      description: 'Sed ut perspiciatis omnis iste natus error sit accusantium doloremque',
-    },
-    {
-      icon: "strength-2.png",
-      title: 'INDUSTRY LEADER',
-      description: 'Sed ut perspiciatis omnis iste natus error sit accusantium doloremque',
-    },
-    {
-      icon: "strength-3.png",
-      title: 'THOUSAND STUDENTS',
-      description: 'Sed ut perspiciatis omnis iste natus error sit accusantium doloremque',
-    },
-  ];
+	customOptions: OwlOptions = {
+		loop: true,
+		mouseDrag: true,
+		touchDrag: true,
+		pullDrag: true,
+		dots: false,
+		autoplay: true,
+		autoplayTimeout: 3000,
+		navSpeed: 100,
+		fluidSpeed: true,
+		lazyLoad: true,
+		navText: ['', ''],
+		responsive: {
+			0: {
+				items: 1,
+			},
+			400: {
+				items: 1,
+			},
+			740: {
+				items: 1,
+			},
+			940: {
+				items: 1,
+			},
+		},
+		nav: true,
+	};
+	noticeData = [
+		{
+			date: {
+				month: 'May',
+				date: 31,
+			},
+			title: 'Memorial day holiday 2021',
+			time: '12:00 AM',
+		},
+		{
+			date: {
+				month: 'June',
+				date: 11,
+			},
+			title: 'Summer A 2021 classes (4 & 8 week) begins',
+			time: '12:00 AM',
+		},
+		{
+			date: {
+				month: 'Sep',
+				date: 25,
+			},
+			title: 'Summer A 2022',
+			time: '12:00 AM',
+		},
+		{
+			date: {
+				month: 'May',
+				date: 31,
+			},
+			title: 'Memorial day holiday 2021',
+			time: '12:00 AM',
+		},
+	];
+	strengthData = [
+		{
+			icon: 'strength-1.png',
+			title: 'EXCELLENT FALCUTY',
+			description:
+				'Sed ut perspiciatis omnis iste natus error sit accusantium doloremque',
+		},
+		{
+			icon: 'strength-2.png',
+			title: 'INDUSTRY LEADER',
+			description:
+				'Sed ut perspiciatis omnis iste natus error sit accusantium doloremque',
+		},
+		{
+			icon: 'strength-3.png',
+			title: 'THOUSAND STUDENTS',
+			description:
+				'Sed ut perspiciatis omnis iste natus error sit accusantium doloremque',
+		},
+	];
 
-  constructor() {
-  }
+	mahaGov = [
+		{
+			image: 'chief_minister.jpg',
+			name: 'Shri Uddhav Thakare',
+			position: "Hon'ble Chief Minister",
+		},
+		{
+			image: 'deputy_chief_minister.jpg',
+			name: 'Shri Ajit Pawar',
+			position: "Hon'ble Deputy Chief Minister",
+		},
+		{
+			image: '',
+			name: '',
+			position: "Hon'ble Cabinet Minister",
+		},
+		{
+			image: '',
+			name: '',
+			position: "Hon'ble Minister of State",
+		},
+	];
 
-  ngOnInit(): void {
-  }
+	constructor() {}
+
+	ngOnInit(): void {}
 }
