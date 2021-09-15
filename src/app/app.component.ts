63f33c286ac09c4e223f6bfda46adcab42ad6183
Akshay Kumar <akshay2796@gmail.com>
2021-09-16 03:45:09 +0530

    Check for blank language + fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'SarthiWeb';
-  notHomeComponent: boolean = true;
-  currentRoute: string = '';
-  constructor(private router: Router) {
-    this.router.events.subscribe((ev) => {
-      if (ev instanceof NavigationEnd) {
-        /* Your code goes here on every router change */
-        this.notHomeComponent = window.location.pathname !== '/';
-        this.currentRoute = window.location.pathname;
-      }
-    });
-  }
+export class AppComponent implements OnInit {
+	title = 'SarthiWeb';
+	notHomeComponent: boolean = true;
+	currentRoute: string = '';
+	constructor(private router: Router) {
+		this.router.events.subscribe((ev) => {
+			if (ev instanceof NavigationEnd) {
+				/* Your code goes here on every router change */
+				this.notHomeComponent = window.location.pathname !== '/';
+				this.currentRoute = window.location.pathname;
+			}
+		});
+	}
+	ngOnInit(): void {
+		const l = localStorage.getItem('lang');
+		if (l === undefined) {
+			localStorage.setItem('lang', 'en');
+		}
+	}
 }
