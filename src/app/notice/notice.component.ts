6dea4e119f08b1fd29493c210d28f6b41c2c265b
Ryan Singh <ryan97singh@gmail.com>
2021-10-18 14:50:47 +0530

    Added - announcements

--- src/app/notice/notice.component.ts
+++ src/app/notice/notice.component.ts
@@ -54,6 +54,31 @@ export class NoticeComponent implements OnInit {
 		},
 		{
 			icon: 'horn.png',
+			title: 'Guidelines',
+			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/GUIDELINES%20FOR%20APPLICANTS_2.pdf',
+		},
+		{
+			icon: 'horn.png',
+			title: 'महत्त्वाच्या सूचना',
+			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/%E0%A4%AE%E0%A4%B9%E0%A4%A4%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B5%E0%A4%BE%E0%A4%9A%E0%A5%8D%E0%A4%AF%E0%A4%BE%20%E0%A4%B8%E0%A5%82%E0%A4%9A%E0%A4%A8%E0%A4%BE_2.pdf',
+		},
+		{
+			icon: 'horn.png',
+			title: 'Checklist',
+			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/CHECKLIST%20-MPSC%20MAINS%20SPONSORSHIP_2.pdf',
+		},
+		{
+			icon: 'horn.png',
+			title: 'Candidate Undertaking',
+			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/CANDIDATE%20UNDERTAKING_1.pdf',
+		},
+		{
+			icon: 'horn.png',
+			title: 'Model Code of Conduct',
+			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/MODEL%20CODE%20OF%20CONDUCT%20-MPSC%20MAINS%20SPONSORSHIP_0.pdf',
+		},
+		{
+			icon: 'horn.png',
 			title: 'UPSC (Civil Services) 2022 Competitive Exam Coaching मुदतवाढ',
 			link: 'http://old.sarthi-maharashtragov.in/en/form/online-application-form-for-cse',
 		},
