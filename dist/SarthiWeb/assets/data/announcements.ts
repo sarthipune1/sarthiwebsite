ebe232921d72a239e197a67691d7a2a88ed68490
Akshay Kumar <akshay2796@gmail.com>
2021-10-18 15:38:35 +0530

    Updated announcements

---
+++ dist/SarthiWeb/assets/data/announcements.ts
@@ -0,0 +1,70 @@
+export type Announcements = {
+	icon: 'new.png' | 'horn.png';
+	title: string;
+	link: string;
+};
+
+const announcements: Announcements[] = [
+	{
+		icon: 'new.png',
+		title: 'MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates) - मुदतवाढ  दि.18.10.2021',
+		link: 'assets/files/MPSC MAINS 2020.pdf',
+	},
+	{
+		icon: 'new.png',
+		title: '(Date Extended till 18th Oct 2021) MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
+		link: 'https://docs.google.com/forms/d/e/1FAIpQLSd5XH-nTUNGJWGcaDtvl_19oTi5ex1n6THwh8klH3oc3y0bdA/viewform?usp=sf_link',
+	},
+	{
+		icon: 'horn.png',
+		title: 'Guidelines - (Date Extended till 18th Oct 2021) MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
+		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/GUIDELINES%20FOR%20APPLICANTS_2.pdf',
+	},
+	{
+		icon: 'horn.png',
+		title: 'महत्त्वाच्या सूचना - (Date Extended till 18th Oct 2021) MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
+		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/%E0%A4%AE%E0%A4%B9%E0%A4%A4%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B5%E0%A4%BE%E0%A4%9A%E0%A5%8D%E0%A4%AF%E0%A4%BE%20%E0%A4%B8%E0%A5%82%E0%A4%9A%E0%A4%A8%E0%A4%BE_2.pdf',
+	},
+	{
+		icon: 'horn.png',
+		title: 'Checklist - (Date Extended till 18th Oct 2021) MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
+		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/CHECKLIST%20-MPSC%20MAINS%20SPONSORSHIP_2.pdf',
+	},
+	{
+		icon: 'horn.png',
+		title: 'Candidate Undertaking - (Date Extended till 18th Oct 2021) MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
+		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/CANDIDATE%20UNDERTAKING_1.pdf',
+	},
+	{
+		icon: 'horn.png',
+		title: 'Model Code of Conduct - (Date Extended till 18th Oct 2021) MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
+		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/MODEL%20CODE%20OF%20CONDUCT%20-MPSC%20MAINS%20SPONSORSHIP_0.pdf',
+	},
+	{
+		icon: 'horn.png',
+		title: 'UPSC (Civil Services) 2022 Competitive Exam Coaching मुदतवाढ',
+		link: 'http://old.sarthi-maharashtragov.in/en/form/online-application-form-for-cse',
+	},
+	{
+		icon: 'horn.png',
+		title: 'MAHARASHTRA ENGINEERING SERVICES (MES) 2021 PROGRAM मुदतवाढ',
+		link: 'http://old.sarthi-maharashtragov.in/en/form/online-application-form-for-mes',
+	},
+	{
+		icon: 'horn.png',
+		title: 'Civil Judge Junior Division & Judicial Magistrate First Class Coaching  2021  मुदतवाढ',
+		link: 'http://old.sarthi-maharashtragov.in/en/form/online-application-form-for-cjjd',
+	},
+	{
+		icon: 'horn.png',
+		title: 'MPSC Subordinate Services (Non-gazetted)Group -B Combined (PSI,ASO,STI) Exam Coaching 2021 मुदतवाढ',
+		link: 'http://old.sarthi-maharashtragov.in/en/form/online-application-form-for-mpsc',
+	},
+	{
+		icon: 'horn.png',
+		title: 'Staff Selection Commission Non Gazetted  Posts Examination Coaching 2021  मुदतवाढ',
+		link: 'http://old.sarthi-maharashtragov.in/en/webform/online_application_form_for_staf',
+	},
+];
+
+export default announcements;
--- src/app/notice/notice.component.ts
+++ src/app/notice/notice.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 // import * as FullEditor from 'ckeditor5-custom-build-full/build/ckeditor.js';
 import * as FullEditor from 'ckeditor5-custom-build/build/ckeditor.js';
+import announcements from 'assets/data/announcements';
 
 @Component({
 	selector: 'app-notice',
@@ -46,63 +47,7 @@ export class NoticeComponent implements OnInit {
 			time: '12:00 AM',
 		},
 	];
-	strengthData = [
-		{
-			icon: 'new.png',
-			title: '(Date Extended till 18th Oct 2021) MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
-			link: 'https://docs.google.com/forms/d/e/1FAIpQLSd5XH-nTUNGJWGcaDtvl_19oTi5ex1n6THwh8klH3oc3y0bdA/viewform?usp=sf_link',
-		},
-		{
-			icon: 'horn.png',
-			title: 'Guidelines',
-			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/GUIDELINES%20FOR%20APPLICANTS_2.pdf',
-		},
-		{
-			icon: 'horn.png',
-			title: 'महत्त्वाच्या सूचना',
-			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/%E0%A4%AE%E0%A4%B9%E0%A4%A4%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B5%E0%A4%BE%E0%A4%9A%E0%A5%8D%E0%A4%AF%E0%A4%BE%20%E0%A4%B8%E0%A5%82%E0%A4%9A%E0%A4%A8%E0%A4%BE_2.pdf',
-		},
-		{
-			icon: 'horn.png',
-			title: 'Checklist',
-			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/CHECKLIST%20-MPSC%20MAINS%20SPONSORSHIP_2.pdf',
-		},
-		{
-			icon: 'horn.png',
-			title: 'Candidate Undertaking',
-			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/CANDIDATE%20UNDERTAKING_1.pdf',
-		},
-		{
-			icon: 'horn.png',
-			title: 'Model Code of Conduct',
-			link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/MODEL%20CODE%20OF%20CONDUCT%20-MPSC%20MAINS%20SPONSORSHIP_0.pdf',
-		},
-		{
-			icon: 'horn.png',
-			title: 'UPSC (Civil Services) 2022 Competitive Exam Coaching मुदतवाढ',
-			link: 'http://old.sarthi-maharashtragov.in/en/form/online-application-form-for-cse',
-		},
-		{
-			icon: 'horn.png',
-			title: 'MAHARASHTRA ENGINEERING SERVICES (MES) 2021 PROGRAM मुदतवाढ',
-			link: 'http://old.sarthi-maharashtragov.in/en/form/online-application-form-for-mes',
-		},
-		{
-			icon: 'horn.png',
-			title: 'Civil Judge Junior Division & Judicial Magistrate First Class Coaching  2021  मुदतवाढ',
-			link: 'http://old.sarthi-maharashtragov.in/en/form/online-application-form-for-cjjd',
-		},
-		{
-			icon: 'horn.png',
-			title: 'MPSC Subordinate Services (Non-gazetted)Group -B Combined (PSI,ASO,STI) Exam Coaching 2021 मुदतवाढ',
-			link: 'http://old.sarthi-maharashtragov.in/en/form/online-application-form-for-mpsc',
-		},
-		{
-			icon: 'horn.png',
-			title: 'Staff Selection Commission Non Gazetted  Posts Examination Coaching 2021  मुदतवाढ',
-			link: 'http://old.sarthi-maharashtragov.in/en/webform/online_application_form_for_staf',
-		},
-	];
+	strengthData = announcements;
 
 	editorConfig = {
 		toolbar: {
