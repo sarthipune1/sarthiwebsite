7aa4bd0c9beb814ab65ee9354cbe29e32d596d86
Ryan Singh <ryan97singh@gmail.com>
2021-10-21 16:22:05 +0530

    Build - announcements closed

--- dist/SarthiWeb/assets/data/announcements.ts
+++ dist/SarthiWeb/assets/data/announcements.ts
@@ -1,10 +1,15 @@
 export type Announcements = {
-	icon: 'new.png' | 'horn.png' | 'old.png';
+	icon: 'new.png' | 'horn.png' | 'old.png' | 'closed.png';
 	title: string;
 	link: string;
 };
 
 const announcements: Announcements[] = [
+	{
+		icon: 'new.png',
+		title: 'विविध समारंभ / कार्यक्रम व्यवस्थापन () एजन्सी निवड करणेकरिता दरपत्रके मागविण्यात येत आहेत. - Call for Quotation for Selection of Event Management Agency in Pune District (Including PCMC) for various Programs organized by SARTHI, Pune.',
+		link: 'assets/files/Event Management Quotation.pdf',
+	},
 	{
 		icon: 'new.png',
 		title: 'छत्रपती शाहू महाराज राष्ट्रीय संशोधन अधिछात्रवृत्ती (CSMNRF -2021) CHHATRAPATI SHAHU MAHARAJ NATIONAL RESEARCH FELLOWSHIP - छत्रपती शाहू महाराज राष्ट्रीय संशोधन अधिछात्रवृत्ती (CSMNRF -2021)  घोषणा new परीपूर्ण अर्जाची यादी, आक्षेप (त्रुटीची पूर्तता करण्याकरीता) यादी व रद्द करण्यात आलेल्या अर्जाची यादी',
@@ -67,37 +72,37 @@ const announcements: Announcements[] = [
 		link: 'http://old.sarthi-maharashtragov.in/en/webform/online_application_form_for_staf',
 	},
 	{
-		icon: 'old.png',
+		icon: 'closed.png',
 		title: ' (Date Extended till 18th Oct 2021) MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates) - मुदतवाढ  दि.18.10.2021',
 		link: 'assets/files/MPSC MAINS 2020.pdf',
 	},
 	{
-		icon: 'old.png',
+		icon: 'closed.png',
 		title: '(Date Extended till 18th Oct 2021) MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
 		link: 'https://docs.google.com/forms/d/e/1FAIpQLSd5XH-nTUNGJWGcaDtvl_19oTi5ex1n6THwh8klH3oc3y0bdA/viewform?usp=sf_link',
 	},
 	{
-		icon: 'old.png',
+		icon: 'closed.png',
 		title: 'Guidelines - MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
 		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/GUIDELINES%20FOR%20APPLICANTS_2.pdf',
 	},
 	{
-		icon: 'old.png',
+		icon: 'closed.png',
 		title: 'महत्त्वाच्या सूचना - MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
 		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/%E0%A4%AE%E0%A4%B9%E0%A4%A4%E0%A5%8D%E0%A4%A4%E0%A5%8D%E0%A4%B5%E0%A4%BE%E0%A4%9A%E0%A5%8D%E0%A4%AF%E0%A4%BE%20%E0%A4%B8%E0%A5%82%E0%A4%9A%E0%A4%A8%E0%A4%BE_2.pdf',
 	},
 	{
-		icon: 'old.png',
+		icon: 'closed.png',
 		title: 'Checklist - MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
 		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/CHECKLIST%20-MPSC%20MAINS%20SPONSORSHIP_2.pdf',
 	},
 	{
-		icon: 'old.png',
+		icon: 'closed.png',
 		title: 'Candidate Undertaking - MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
 		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/CANDIDATE%20UNDERTAKING_1.pdf',
 	},
 	{
-		icon: 'old.png',
+		icon: 'closed.png',
 		title: 'Model Code of Conduct - MPSC MAINS 2020 Sponsorship (for MPSC Prelims 2020 Pass Candidates)',
 		link: 'http://old.sarthi-maharashtragov.in/sites/default/files/inline-files/MODEL%20CODE%20OF%20CONDUCT%20-MPSC%20MAINS%20SPONSORSHIP_0.pdf',
 	},
