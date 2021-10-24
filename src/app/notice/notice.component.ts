4d2d82b5320dd3af51f350920648e1be12a04297
Ryan Singh <ryan97singh@gmail.com>
2021-10-24 13:43:52 +0530

    changed notice layout

--- src/app/notice/notice.component.ts
+++ src/app/notice/notice.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 // import * as FullEditor from 'ckeditor5-custom-build-full/build/ckeditor.js';
 import * as FullEditor from 'ckeditor5-custom-build/build/ckeditor.js';
 import announcements from 'assets/data/announcements';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
 	selector: 'app-notice',
@@ -9,6 +10,35 @@ import announcements from 'assets/data/announcements';
 	styleUrls: ['./notice.component.scss'],
 })
 export class NoticeComponent implements OnInit {
+	modalVisible: boolean = false;
+
+	currentCategory: string = '';
+	getCategoryContent = [
+		{
+			icon: 'new.png',
+			category:
+				'MPSC (STATE SERVICES) 2021-22 COACHING FIRST WAITING LIST',
+			title: 'MPSC (STATE SERVICES) 2021-22 COACHING FIRST WAITING LIST',
+			link: 'http://old.sarthi-maharashtragov.in/en/node/4',
+		},
+		{
+			icon: 'new.png',
+			category:
+				'UPSC (Civil Services) 2022 Competitive Exam Coaching मुदतवाढ',
+			title: 'UPSC (Civil Services) 2022 Competitive Exam Coaching मुदतवाढ',
+			link: 'assets/files/UPSC 2022.pdf',
+		},
+		{
+			icon: 'new.png',
+			category:
+				'MAHARASHTRA ENGINEERING SERVICES (MES) 2021 PROGRAM मुदतवाढ',
+			title: 'MAHARASHTRA ENGINEERING SERVICES (MES) 2021 PROGRAM मुदतवाढ',
+			link: 'assets/files/CJJD & JMFC.pdf',
+		},
+	];
+	faIcons = {
+		faTimes,
+	};
 	constructor() {}
 	public Editor = FullEditor;
 
@@ -47,6 +77,16 @@ export class NoticeComponent implements OnInit {
 			time: '12:00 AM',
 		},
 	];
+
+	toggleModal() {
+		this.modalVisible = !this.modalVisible;
+	}
+
+	openCategory(category: string) {
+		this.currentCategory = category;
+		// HTTP call to fetch notices
+		this.toggleModal();
+	}
 	strengthData = announcements;
 
 	editorConfig = {
