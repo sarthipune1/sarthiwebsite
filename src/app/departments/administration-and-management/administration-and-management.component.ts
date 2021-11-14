b6c50b73c440f06f7b2880066c3ff200ae6629e5
Akshay Kumar <akshay2796@gmail.com>
2021-11-15 01:18:14 +0530

    Administration department

--- src/app/departments/administration-and-management/administration-and-management.component.ts
+++ src/app/departments/administration-and-management/administration-and-management.component.ts
@@ -1,12 +1,52 @@
 import { Component, OnInit } from '@angular/core';
+import obj from '../../../assets/data/exam';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { HttpClient } from '@angular/common/http';
+import { apiUrl } from 'assets/data/environment';
 
+export interface Tile {
+	text: string;
+	sr: number;
+	download: any;
+}
+
+interface Exam {
+	id: number;
+	title: string;
+	content: string;
+}
 @Component({
 	selector: 'app-administration-and-management',
 	templateUrl: './administration-and-management.component.html',
 	styleUrls: ['./administration-and-management.component.scss'],
 })
 export class AdministrationAndManagementComponent implements OnInit {
-	constructor() {}
+	modalVisible: boolean = false;
+	selectedExam: Exam;
+	exams: Exam[] = [];
+	baseUrl: string = apiUrl + '/department/admin';
+	faIcons = {
+		faTimes,
+	};
 
-	ngOnInit(): void {}
+	openModal(exam: Exam) {
+		this.selectedExam = exam;
+		this.toggleModal();
+	}
+
+	toggleModal() {
+		this.modalVisible = !this.modalVisible;
+	}
+
+	constructor(private http: HttpClient) {}
+
+	ngOnInit(): void {
+		this.getExams();
+	}
+
+	getExams() {
+		this.http.get<Exam[]>(this.baseUrl).subscribe((data) => {
+			this.exams = data;
+		});
+	}
 }
--- src/assets/data/environment.ts
+++ src/assets/data/environment.ts
@@ -1,4 +1,4 @@
-// const apiUrl: string = 'https://api.sarthi-maharashtragov.in';
-const apiUrl: string = 'http://localhost:3001/api';
+const apiUrl: string = 'https://api.sarthi-maharashtragov.in';
+// const apiUrl: string = 'http://localhost:3001/api';
 
 export { apiUrl };
