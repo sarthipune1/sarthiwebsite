7e4fc7bab4e99b871585a17629b0a54568304cfc
Akshay Kumar <akshay2796@gmail.com>
2021-12-09 03:15:18 +0530

    Mission, Vision, Awards

--- src/app/awards/awards.component.ts
+++ src/app/awards/awards.component.ts
@@ -1,6 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { RouteService } from 'app/services/route.service';
 import { Subheader } from 'app/subheader/subheader.component';
+import { environment } from 'environments/environment';
+
+export interface IAwards {
+	id: number;
+	title: string;
+	link: string;
+	url: string;
+	createdAt: string;
+	updatedAt: string;
+}
 
 @Component({
 	selector: 'app-awards',
@@ -8,12 +19,25 @@ import { Subheader } from 'app/subheader/subheader.component';
 	styleUrls: ['./awards.component.scss'],
 })
 export class AwardsComponent implements OnInit {
-	constructor(private routeService: RouteService) {}
+	constructor(private http: HttpClient, private routeService: RouteService) {}
 
 	pageStats: Subheader;
+	awards: IAwards[] = [];
+
+	get url() {
+		return environment.apiUrl;
+	}
+	private readonly baseUrl: string = this.url + '/' + 'awards';
 	ngOnInit(): void {
 		this.routeService.onGetData.subscribe((pageStats: Subheader) => {
 			this.pageStats = pageStats;
+		});
+		this.getAwards();
+	}
+
+	getAwards() {
+		this.http.get<IAwards[]>(this.baseUrl).subscribe((awards) => {
+			this.awards = awards;
 		});
 	}
 }
